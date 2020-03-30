8cc44265dfac71341f149517e663b81cb70cc0b1
lc <lc.apps.dev@gmail.com>
2020-03-30 16:50:22 +0200

    first commit

---
+++ src/main/java/pl/lc/notes/FamilyNotesBaseApplication.java
@@ -0,0 +1,13 @@
+package pl.lc.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyNotesBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyNotesBaseApplication.class, args);
+	}
+
+}
---
+++ src/main/java/pl/lc/notes/NotesWebservice.java
@@ -0,0 +1,15 @@
+package pl.lc.notes;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class NotesWebservice {
+	
+	@RequestMapping("/hello-world/{message}")
+	public String test(@PathVariable String message) {
+		return "Hello from server! Message: " + message;
+	}
+}
