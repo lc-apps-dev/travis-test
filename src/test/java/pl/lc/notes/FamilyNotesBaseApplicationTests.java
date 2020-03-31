80d48fed9e90fb10d9ddca68457abf68bfcfe54e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-31 16:53:13 +0000

    Deploy lc-apps-dev/family-note to github.com/lc-apps-dev/family-note.git:gh-pages

---
+++ src/test/java/pl/lc/notes/FamilyNotesBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lc.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyNotesBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
